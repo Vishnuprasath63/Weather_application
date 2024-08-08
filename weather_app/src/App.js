5e57b69dad5a55f4b3e67d8e3cc0570fd81eccc1
Vishnuprasath63 <145005255+Vishnuprasath63@users.noreply.github.com>
2024-08-08 11:55:17 +0530

    Update App.js

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -76,6 +76,7 @@ function GfGWeatherApp() {
 		<div className="App">
 			<h1 className="app-name">
 				All in one weather appli
+				weather app check
 			</h1>
 			<div className="search-bar">
 				<input
